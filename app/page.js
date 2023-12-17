4127848497f4461579916a1c42cb6881e5e5b57a
beauty <wjdqbxl@gachon.ac.kr>
2023-12-17 23:23:34 +0900

    #1 FRAMEWORK OVERVIEW > #1.6 Custom App --- - style jsx global은 페이지 단위로 적용됨   - page.js(index)에서 global로 span 스타일을 작성함 --> index페이지의 NavBar의 span도 적용됨, 하지만 about페이지에 있는 NavBar span은 적용되지 않음

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
 import NavBar from "@/components/NavBar";
@@ -6,7 +7,12 @@ export default function Home() {
   return (
       <div>
           <NavBar />
-          <h1>Hello</h1>
+          <h1 className="active">Hello</h1>
+          <style jsx global>{`
+              span{
+                color: white;
+              }
+          `}</style>
       </div>
   )
 }
